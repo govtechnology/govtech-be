dc370fc433eec772e627b5ff1aaaa2af7290ca59
xyzuan <jodyyuan@xyzuan.my.id>
2023-11-12 07:14:27 +0000

    main: certificate: Add more skType

--- src/controller/certificate.controller.js
+++ src/controller/certificate.controller.js
@@ -39,6 +39,62 @@ export const requestCertificate = async (req, res, next) => {
                     });
                 }
                 break;
+            case 'SKMS':
+                if (!skData.nama || !skData.ttl || !skData.nik || !skData.alamat || !skData.usaha || !skData.jenisAlat || !skData.jumlahAlat || !skData.fungsiAlat || !skData.jenisBBM || !skData.kodeSPBU || !skData.lokasiSPBU) {
+                    return res.status(400).json({
+                        success: false,
+                        error: 'Missing required payload fields: nama, ttl, nik, alamat, usaha, jenisAlat, jumlahAlat, fungsiAlat, jenisBBM, kodeSPBU, and lokasiSPBU',
+                    });
+                }
+                break;
+            case 'SKD':
+                if (!skData.nama || !skData.nik || !skData.ttl || !skData.agama || !skData.kelamin || !skData.status || !skData.pekerjaan || !skData.alamat) {
+                    return res.status(400).json({
+                        success: false,
+                        error: 'Missing required payload fields: nama, nik, ttl, agama, kelamin, status, pekerjaan, and alamat',
+                    });
+                }
+                break;
+            case 'SKU':
+                if (!skData.nama || !skData.nik || !skData.ttl || !skData.kelamin || !skData.alamat || !skData.agama || !skData.status || !skData.pendidikan || !skData.pekerjaan || !skData.usaha) {
+                    return res.status(400).json({
+                        success: false,
+                        error: 'Missing required payload fields: nama, nik, ttl, kelamin, alamat, agama, status, pendidikan, pekerjaan, and usaha',
+                    });
+                }
+                break;
+            case 'SKK':
+                if (!skData.nama || !skData.jenisKelamin || !skData.alamat || !skData.umur || !skData.hariMeninggal || !skData.tanggalMeninggal || !skData.lokasiMeninggal || !skData.sebab) {
+                    return res.status(400).json({
+                        success: false,
+                        error: 'Missing required payload fields: nama, jenisKelamin, alamat, umur, hariMeninggal, tanggalMeninggal, lokasiMeninggal, and sebab',
+                    });
+                }
+                break;
+            case 'SKPB':
+                if (!skData.nama || !skData.nik || !skData.ttl || !skData.kelamin || !skData.alamat || !skData.agama || !skData.status || !skData.pendidikan || !skData.pekerjaan || !skData.usaha || !skData.bank) {
+                    return res.status(400).json({
+                        success: false,
+                        error: 'Missing required payload fields: nama, nik, ttl, kelamin, alamat, agama, status, pendidikan, pekerjaan, usaha, and bank',
+                    });
+                }
+                break;
+            case 'SKHIL':
+                if (!skData.nama || !skData.nik || !skData.ttl || !skData.kelamin || !skData.alamat || !skData.agama || !skData.status || !skData.pendidikan || !skData.pekerjaan || !skData.hilang || !skData.keterangan) {
+                    return res.status(400).json({
+                        success: false,
+                        error: 'Missing required payload fields: nama, nik, ttl, kelamin, alamat, agama, status, pendidikan, pekerjaan, hilang, and keterangan',
+                    });
+                }
+                break;
+            case 'SKCK':
+                if (!skData.nama || !skData.nik || !skData.ttl || !skData.agama || !skData.kelamin || !skData.alamat || !skData.status || !skData.pendidikan || !skData.pekerjaan || !skData.keperluan) {
+                    return res.status(400).json({
+                        success: false,
+                        error: 'Missing required payload fields: nama, nik, ttl, agama, kelamin, alamat, status, pendidikan, pekerjaan, keperluan',
+                    });
+                }
+                break;
             default:
                 return res.status(400).json({
                     success: false,
