c2371151abad380df8ef508a9f86edaa57be4316
root <root@LAPTOP-8V28TFLO>
2023-12-12 03:25:30 +0700

    feature(session-management): Backup dbConnector.js

--- src/lib/dbConnector.js
+++ src/lib/dbConnector.js
@@ -1,10 +1,6 @@
 const mysql = require("mysql2/promise");
 
 export const sqldb = mysql.createPool({
-  host: '192.168.112.1',
-  port: 3306,
-  user: 'ngubalan',
-  password: 'ngubalan123',
-  database: 'test',
-  waitForConnections: false
+  uri: process.env.DATABASE_URL,
+  waitForConnections: true,
 });
